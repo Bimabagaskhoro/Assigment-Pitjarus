4ae28289d36ead5c98cf153155d4b8462d0a1972
bima bagaskhoro <bima.bagaskhoro@gmail.com>
2022-06-08 14:07:52 +0700

    prototype ui

---
+++ app/src/main/java/com/bimabagaskhoro/pitjarustestapp/ListStoreActivity.kt
@@ -0,0 +1,11 @@
+package com.bimabagaskhoro.pitjarustestapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListStoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_store)
+    }
+}
